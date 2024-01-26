46c96b4877d066431f2b77789f3ce3afc861a43e
Seung taek Oh <elqkddl@naver.com>
2024-01-26 20:42:11 +0900

    [Feat] Edit을 누르면 Player1 이 Text창으로 변경(useState)

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,14 +1,28 @@
 // props 를 넣는 이유는 Player 1 과 X라는 것을 매번 하드코딩으로 바꾸는 것을 피하기 위해 사용
 // props 받은 것을 부모한테 넘겨줌
 
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+
+  let playerName = <span className="player-name">{name}</span>;
+
+  if (isEditing) {
+    playerName = <input type="text" required />;
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {playerName}
         <span className="player-symol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>Edit</button>
     </li>
   );
 }
