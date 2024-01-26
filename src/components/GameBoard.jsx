911442d7419c2fc61810265700fef8c59574b83d
Seung taek Oh <elqkddl@naver.com>
2024-01-26 22:12:08 +0900

    [Feat] 다차원 리스트 렌더링

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,23 @@
+const initialGameBaord = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+export default function GameBoard() {
+  return (
+    <ol id="game-board">
+      {initialGameBaord.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button>{playerSymbol}</button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
