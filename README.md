183dae140c7ebe59f121aaac640a8ae4417eeacc
Seung taek Oh <elqkddl@naver.com>
2024-01-29 16:33:11 +0900

    [Feat] 불변의 객체 State로 상태 업데이트 하기

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Tic-Tac-Toe with React
 
 > React 기초 다지기 공부용 프로젝트
+> React로 대화형 틱택토 게임을 빌드해 보이는 것.
 > Demo :
 
 ## 실행
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialGameBaord = [
   [null, null, null],
   [null, null, null],
@@ -5,14 +7,23 @@ const initialGameBaord = [
 ];
 
 export default function GameBoard() {
+  const [gameboard, setGameBoard] = useState(initialGameBaord);
+
+  const handleGameBoard = (rowIndex, colIndex) => {
+    setGameBoard((preGameBoard) => {
+      const updatedBoard = [...preGameBoard.map((innerArray) => [...innerArray])];
+      updatedBoard[rowIndex][colIndex] = "X";
+      return updatedBoard;
+    });
+  };
   return (
     <ol id="game-board">
-      {initialGameBaord.map((row, rowIndex) => (
+      {gameboard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleGameBoard(rowIndex, colIndex)}>{playerSymbol}</button>
               </li>
             ))}
           </ol>
