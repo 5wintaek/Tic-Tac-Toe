ec59f7a3ad0f407ca451560a50b9ae4ae6395b64
Seung taek Oh <elqkddl@naver.com>
2024-02-06 15:46:18 +0900

    [Feat] 행,열 조합하여 우승자 확인하기

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import Player from './components/Player';
 import GameBoard from './components/GameBoard';
 import Log from './components/Log';
 import {useState} from 'react';
+import {WINNING_COMBINATIONS} from './data';
+const initialGameBaord = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
 // 컴포넌트 함수가 재실행될 때 스스로 재실행되지 않음
 const deriveActivePlayer = (gameTurns) => {
@@ -16,9 +22,31 @@ const deriveActivePlayer = (gameTurns) => {
 
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
+
   // const [activePlayer, setActivePlayer] = useState('X');
   // activePlayer 는 gameTurns 안에 넣어서 변경 가능하기 떄문에 불필요한 State 상태 제거
   const activePlayer = deriveActivePlayer(gameTurns);
+  let gameBoard = initialGameBaord;
+
+  for (const turn of gameTurns) {
+    const {square, player} = turn;
+    const {row, col} = square;
+
+    gameBoard[row][col] = player;
+  }
+
+  let winner;
+
+  for (const combination of WINNING_COMBINATIONS) {
+    // 행2 열2 조합
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
+
+    if (firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol) {
+      winner = firstSquareSymbol;
+    }
+  }
 
   const handleSelectSquare = (row, col) => {
     // setActivePlayer((curActivePlayer) => (curActivePlayer === 'X' ? 'O' : 'X'));
@@ -41,7 +69,8 @@ function App() {
           <Player startName="Player1" symbol="X" isActive={activePlayer === 'X'} />
           <Player startName="Player2" symbol="O" isActive={activePlayer === 'O'} />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+        {winner && <p>You won, {winner} ! </p>}
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
     </main>
