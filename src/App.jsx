130dcd3d16d02bcb1289ead8c5612471e7b46598
Seung taek Oh <elqkddl@naver.com>
2024-02-06 10:57:40 +0900

    [Feat] 불필요한 State상태 관리 제거(activePlayer)

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,30 @@ import GameBoard from './components/GameBoard';
 import Log from './components/Log';
 import {useState} from 'react';
 
+// 컴포넌트 함수가 재실행될 때 스스로 재실행되지 않음
+const deriveActivePlayer = (gameTurns) => {
+  let currentPlayer = 'X';
+
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayer = 'O';
+  }
+
+  return currentPlayer;
+};
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState('X');
+  // const [activePlayer, setActivePlayer] = useState('X');
+  // activePlayer 는 gameTurns 안에 넣어서 변경 가능하기 떄문에 불필요한 State 상태 제거
+  const activePlayer = deriveActivePlayer(gameTurns);
 
   const handleSelectSquare = (row, col) => {
-    setActivePlayer((curActivePlayer) => (curActivePlayer === 'X' ? 'O' : 'X'));
+    // setActivePlayer((curActivePlayer) => (curActivePlayer === 'X' ? 'O' : 'X'));
     setGameTurns((prevTurns) => {
-      let currentPlayer = 'X';
+      const currentPlayer = deriveActivePlayer(prevTurns);
 
-      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
-        currentPlayer = 'O';
-      }
+      console.log('이전 턴 정보:', prevTurns);
+
       // 이미 생성된 배열을 복사 하는 이유는 불변하는 방식으로 해야하기 때문에
       const updatedTurns = [{square: {row: row, col: col}, player: currentPlayer}, ...prevTurns];
 
