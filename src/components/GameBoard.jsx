b2642328bf2e9c22b68f5772070100bb53fb9d7e
Seung taek Oh <elqkddl@naver.com>
2024-02-06 15:45:36 +0900

    [Feat] disabled 버튼추가 및 O,X 데이터 추가

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,38 +1,15 @@
-const initialGameBaord = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
 // turns 는 배열
-export default function GameBoard({onSelectSquare, turns}) {
-  let gameBoard = initialGameBaord;
-
-  for (const turn of turns) {
-    const {square, player} = turn;
-    const {row, col} = square;
-
-    gameBoard[row][col] = player;
-  }
-  // const [gameboard, setGameBoard] = useState(initialGameBaord);
-
-  // const handleGameBoard = (rowIndex, colIndex) => {
-  //   setGameBoard((preGameBoard) => {
-  //     const updatedBoard = [...preGameBoard.map((innerArray) => [...innerArray])];
-  //     updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-  //     return updatedBoard;
-  //   });
-
-  //   onSelectSquare();
-  // };
+export default function GameBoard({onSelectSquare, board}) {
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled={playerSymbol !== null}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
