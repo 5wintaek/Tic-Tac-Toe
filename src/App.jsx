06d5a6996cbb09d7d6fff2b1b10aad9d495dbef4
Seung taek Oh <elqkddl@naver.com>
2024-02-20 16:23:41 +0900

    [Feat] Header 및 UserInput(section) 스케치

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Header from './components/Headers';
+import UserInput from './components/UserInput';
 function App() {
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header />
+      <UserInput />
+    </>
+  );
 }
 
-export default App
+export default App;
