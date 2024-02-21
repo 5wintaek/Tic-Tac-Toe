9a705232dbc53a992bee2f7b5e3cbfa6ccb7cfb7
Seung taek Oh <elqkddl@naver.com>
2024-02-21 14:17:21 +0900

    [Feat] userInput 상태를 userInput컴포넌트로부터 APP컴포넌트로 상태끌어올림

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
+import {useState} from 'react';
 import Header from './components/Headers';
+import Result from './components/Result';
 import UserInput from './components/UserInput';
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    initInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  // inputIdentifer = userInput 중 하나라 string으로 매개변수 전달해야함
+  function handleChange(inputIdentifer, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifer]: newValue,
+      };
+    });
+  }
+
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput userInput={userInput} onChangeInput={handleChange} />
+      <Result />
     </>
   );
 }
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,26 +1,7 @@
-import {useState} from 'react';
-
-export default function UserInput() {
-  const [userInput, setUserInput] = useState({
-    initInvestment: 10000,
-    annualInvestment: 1200,
-    expectedReturn: 6,
-    duration: 10,
-  });
-
-  // inputIdentifer = userInput 중 하나라 string으로 매개변수 전달해야함
-  function handleChange(inputIdentifer, newValue) {
-    setUserInput((prevUserInput) => {
-      return {
-        ...prevUserInput,
-        [inputIdentifer]: newValue,
-      };
-    });
-  }
-
-  function handleChangeTarget(e) {
-    handleChange('initInvestment', e.target.value);
-  }
+export default function UserInput({onChangeInput, userInput}) {
+  // function handleChangeTarget(e) {
+  //   handleChange('initInvestment', e.target.value);
+  // }
 
   return (
     <section id="user-input">
@@ -31,7 +12,7 @@ export default function UserInput() {
             type="number"
             required
             value={userInput.initInvestment} // 초기의 값을 전달
-            onChange={handleChangeTarget}
+            onChange={(e) => onChangeInput('initInvestment', e.target.value)}
           />
         </p>
         <p>
@@ -40,7 +21,7 @@ export default function UserInput() {
             type="number"
             required
             value={userInput.annualInvestment} // 초기의 값을 전달
-            onChange={(e) => handleChange('annualInvestment', e.target.value)}
+            onChange={(e) => onChangeInput('annualInvestment', e.target.value)}
           />
         </p>
       </div>
@@ -51,7 +32,7 @@ export default function UserInput() {
             type="number"
             required
             value={userInput.expectedReturn} // 초기의 값을 전달
-            onChange={(e) => handleChange('expectedReturn', e.target.value)}
+            onChange={(e) => onChangeInput('expectedReturn', e.target.value)}
           />
         </p>
         <p>
@@ -60,7 +41,7 @@ export default function UserInput() {
             type="number"
             required
             value={userInput.duration} // 초기의 값을 전달
-            onChange={(e) => handleChange('duration', e.target.value)}
+            onChange={(e) => onChangeInput('duration', e.target.value)}
           />
         </p>
       </div>
